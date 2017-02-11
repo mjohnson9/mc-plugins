26e7d49594dcb1b4615310344944ef4ea0f78d1a
Michael Johnson <michael@johnson.computer>
2017-02-11 12:50:31 -0500

    Update all copyrights to include the module names

--- random-respawn/src/main/java/computer/johnson/minecraft/randomrespawn/RandomRespawn.java
+++ random-respawn/src/main/java/computer/johnson/minecraft/randomrespawn/RandomRespawn.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of mc-plugins.
+ * This file is part of mc-plugins-random-respawn.
  *
- * mc-plugins is free software: you can redistribute it and/or modify
+ * mc-plugins-random-respawn is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * mc-plugins is distributed in the hope that it will be useful,
+ * mc-plugins-random-respawn is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with mc-plugins.  If not, see <http://www.gnu.org/licenses/>.
+ * along with mc-plugins-random-respawn.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package computer.johnson.minecraft.randomrespawn;
